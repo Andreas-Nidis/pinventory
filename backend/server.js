669c66bbf36b78257cabeef1a4426ec142e2a29f
Andreas Kalketenidis <andreas.nidis@MacBook-Air-2.local>
2025-02-27 15:42:20 +0100

    Set up api/products route and organised files accordingly.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import productRoutes from "./routes/productRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,10 +16,7 @@ app.use(cors()); //Avoid cors errors
 app.use(helmet()); //Helmet is a security middleware that helps protect your application by setting HTTP headers.
 app.use(morgan("dev")); //This will log requests
 
-app.get("/test", (req, res) => {
-    console.log(res.getHeaders());
-    res.send("Hello from the test route");
-})
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
