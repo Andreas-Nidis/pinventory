1fb04e737e3eec40a71943f3c264cd3be23373c3
Andreas Kalketenidis <andreas.nidis@MacBook-Air-2.local>
2025-03-05 23:12:46 +0100

    Fixed shopping bag and item amount display by moving the amount up.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import { ShoppingCartIcon } from "lucide-react";
+import { Link, useResolvedPath } from 'react-router-dom';
+import { ShoppingCartIcon, ShoppingBagIcon } from "lucide-react";
 
 function Navbar() {
+  const {pathname} = useResolvedPath();
+  const isHomePage = pathname === '/';
+
   return (
     <div className='bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50'>
       <div className='max-w-7xl mx-auto'>
@@ -18,7 +21,23 @@ function Navbar() {
                 PORTSTORE
               </span>
             </div>
-            
+
+          </div>
+
+          {/* SHOPPING CART */}
+          <div className='flex gap-4 items-center ml-auto'>
+
+            {isHomePage && (
+              <div className='indicator'>
+                <div className='p-2 rounded-full hover:bg-base-200 flex'>
+                  <ShoppingBagIcon className='size-5'/>
+                  <div className='-translate-y-3'>
+                    <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">8{/* {products.length} */}</span>
+                  </div>
+                </div>
+              </div>
+            )}
+
           </div>
 
         </div>
