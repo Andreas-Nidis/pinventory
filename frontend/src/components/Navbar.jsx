6334a3a792f73db1b32aecb8ccb6bde1b7aae032
Andreas Kalketenidis <andreas.nidis@MacBook-Air-2.local>
2025-03-10 09:30:21 +0100

    Set up useProductStore.js with an async function to fetch products.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <div className='bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50'>
       <div className='max-w-7xl mx-auto'>
-        <div className='navbar px-4 min-h-[4rem] flex items-center'>
+        <div className='navbar px-16 min-h-[4rem] flex items-center'>
           {/* Logo */}
           <div className='flex-1 lg:flex-none'>
             <Link to='/' className='hover:opacity-80 transition-opactiy' />
@@ -32,7 +32,7 @@ function Navbar() {
                 <div className='p-2 rounded-full hover:bg-base-200 flex'>
                   <ShoppingBagIcon className='size-5'/>
                   <div className='-translate-y-3'>
-                    <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">8{/* {products.length} */}</span>
+                    <span className="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">8{/* {products.length} */}</span>
                   </div>
                 </div>
               </div>
